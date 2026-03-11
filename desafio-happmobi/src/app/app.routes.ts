15c9d2b2537b5c935bedb8688140e106c8fd3551
Eduardo Bellomo <etavares.dev@gmail.com>
2026-03-11 09:30:53 -0300

    - criado componente de login; - criado componente de autenticação; - configurado as rotas;

--- desafio-happmobi/src/app/app.routes.ts
+++ desafio-happmobi/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',
+    redirectTo:'auth/login',
+    pathMatch:'full'
+  },
+  {path: 'auth',
+    loadChildren: () => import ('./layouts/auth-layout/auth-layout.routing').then(r => r.AuthLayoutRoutes)
+  }
+];
